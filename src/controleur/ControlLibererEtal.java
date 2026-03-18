b9d679fd349cb60fee25fc432b3f7372a3f79fe0
AurelienBascobert <aurelien.bascobert31@gmail.com>
2026-03-18 11:58:40 +0100

    fin tp2 -6 non fini

--- src/controleur/ControlLibererEtal.java
+++ src/controleur/ControlLibererEtal.java
@@ -1,29 +1,36 @@
 package controleur;
 
+import villagegaulois.Etal;
+
 public class ControlLibererEtal {
-	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
+    private final ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 
-	public ControlLibererEtal(
-			ControlTrouverEtalVendeur controlTrouverEtalVendeur) {
-		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
-	}
+    public ControlLibererEtal(
+            ControlTrouverEtalVendeur controlTrouverEtalVendeur) {
+        this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
+    }
 
-	//TODO a completer
+    public boolean isVendeur(String nomVendeur) {
+        return controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur) != null;
+    }
 
-	/**
-	 * 
-	 * @param nomVendeur
-	 * @return donneesEtal est un tableau de chaine contenant
-	 * 		[0] : un boolean indiquant si l'étal est occupé
-	 * 		[1] : nom du vendeur
-	 * 		[2] : produit vendu
-	 * 		[3] : quantité de produit à vendre au début du marché
-	 * 		[4] : quantité de produit vendu
-	 */
-	public String[] libererEtal(String nomVendeur) {
-		//TODO a completer
-		String[] donneesEtal = null;
-		return donneesEtal;
-	}
+    /**
+     *
+     * @param nomVendeur
+     * @return donneesEtal est un tableau de chaine contenant
+     * [0] : un boolean indiquant si l'étal est occupé
+     * [1] : nom du vendeur
+     * [2] : produit vendu
+     * [3] : quantité de produit à vendre au début du marché
+     * [4] : quantité de produit vendu
+     */
+    public String[] libererEtal(String nomVendeur) {
+        Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+        String[] donneesEtal = {"rt",
+                nomVendeur,
+                etal.getProduit(),
+                etal.getQuantite()};
+        return donneesEtal;
+    }
 
 }
